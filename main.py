9806b98139adcda6dc89311d44e44a63f0442351
Giovanny Yera <giovannyyera.mo@gmail.com>
2026-09-02 17:10:07 -0300

    feat: organiza menu principal do MaxLanguage

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from services.idioma_service import cadastrar_idioma, buscar_idioma, carregar_indice_idiomas, listar_idiomas
-from seed.dados_iniciais import carregar_idiomas_iniciais, carregar_licoes_iniciais, carregar_exercicios_iniciais
-from services.licao_service import buscar_licao_com_idioma, carregar_indice_licoes
-from services.exercicio_service import carregar_indice_exercicios, buscar_exercicio_com_idioma, listar_exercicios
-from services.usuario_service import carregar_indice_usuarios, cadastrar_usuario, listar_usuarios, buscar_usuario_com_idioma, buscar_usuario
-from services.pratica_service import pode_praticar_exercicio, responder_exercicio
-# import os
+from services.idioma_service import carregar_indice_idiomas, buscar_idioma, listar_idiomas
+from services.licao_service import carregar_indice_licoes, buscar_licao
+from services.exercicio_service import carregar_indice_exercicios, listar_exercicios
+from services.usuario_service import carregar_indice_usuarios, cadastrar_usuario, buscar_usuario, buscar_usuario_com_idioma, listar_usuarios, gerar_ranking, excluir_usuario
+from services.pratica_service import responder_exercicio, finalizar_rodada
+
+from seed.dados_iniciais import (
+    carregar_idiomas_iniciais,
+    carregar_licoes_iniciais,
+    carregar_exercicios_iniciais,
+)
 
 raiz_idiomas = carregar_indice_idiomas()
 raiz_licoes = carregar_indice_licoes()
 raiz_exercicios = carregar_indice_exercicios()
 raiz_usuarios = carregar_indice_usuarios()
+
 
 if raiz_idiomas is None:
     raiz_idiomas = carregar_idiomas_iniciais(raiz_idiomas)
@@ -20,173 +25,365 @@ if raiz_licoes is None:
 if raiz_exercicios is None:
     raiz_exercicios = carregar_exercicios_iniciais(raiz_exercicios, raiz_licoes)
 
-# codigo = int(input("Código do idioma: "))
-# descricao = input("Descrição do idioma: ")
+while True:
+    print("\n- MAXLANGUAGE -")
+    print("1 - Cadastrar usuário")
+    print("2 - Buscar usuário")
+    print("3 - Listar usuários")
+    print("4 - Praticar exercícios")
+    print("5 - Ver ranking")
+    print("6 - Excluir usuário")
+    print("7 - Sair")
 
-# raiz_idiomas = cadastrar_idioma(
-#     raiz_idiomas,
-#     codigo,
-#     descricao
-# )
+    opcao = input("\nEscolha uma opção: ")
+    if opcao not in ["1", "2", "3", "4", "5", "6", "7"]:
+        print("\nOpção inválida. Digite uma opção de 1 a 7.")
+        continue
 
-# print("Idioma cadastrado com sucesso!")
+    if opcao == "1":
+        print("\n CADASTRAR USUÁRIO ")
 
-idiomas = listar_idiomas(raiz_idiomas, [])
-print("\nIdiomas disponíveis: ")
+        while True:
+            try:
+                codigo_usuario = int(input("Digite o código do usuário: "))
+            except ValueError:
+                print("\nDigite apenas números.\n")
+                continue
 
-for idioma in idiomas:
-    print(idioma.codigo, "-", idioma.descricao)
+            usuario_existente = buscar_usuario(raiz_usuarios, codigo_usuario)
 
-codigo_busca = int(
-    input("\nDigite o código para buscar: ")
-)
-idioma = buscar_idioma(
-    raiz_idiomas,
-    codigo_busca
-)
+            if usuario_existente is not None:
+                print("\nEsse código de usuário já existe. Digite outro código.\n")
+            else:
+                break
 
-if idioma is not None:
-    print("\nIdioma encontrado:")
-    print("Código:", idioma.codigo)
-    print("Descrição:", idioma.descricao)
+        while True:
+            nome_usuario = input("Digite o nome do usuário: ").strip()
 
-else:
-    print("\nIdioma não encontrado!")
+            if nome_usuario: 
+                break
 
-codigo_usuario = int(input("\nDigite o código do usuário: "))
+            print("\nO nome não pode ficar vazio.")
 
-usuario_existente = buscar_usuario(
-    raiz_usuarios,
-    codigo_usuario
-)
+        idiomas = listar_idiomas(raiz_idiomas, [])
+        print("\nIdiomas disponíveis: ")
 
-if usuario_existente is not None:
-    print("\nCódigo de usuário já existente!")
+        for idioma in idiomas:
+            print(idioma.codigo, "-", idioma.descricao)
 
-else:
-    nome_usuario = input("Digite o nome do usuário: ")
+        while True:
+            try:
+                codigo_idioma = int(input("\nEscolha o idioma: "))
+            except ValueError:
+                print("\nDigite apenas o número do idioma.")
+                continue
 
-    codigo_idioma = int(
-        input("Digite o código do idioma que deseja aprender: ")
-    )
+            idioma = buscar_idioma(raiz_idiomas, codigo_idioma)
 
-    raiz_usuarios = cadastrar_usuario(
-        raiz_usuarios,
-        raiz_idiomas,
-        codigo_usuario,
-        nome_usuario,
-        codigo_idioma
-    )
+            if idioma is None:
+                print("\nOpção de idioma inválida. Escolha novamente.")
+            else:
+                break
 
-codigo_usuario_busca = int(input("\nDigite o código do usuário para buscar: "))
+        raiz_usuarios = cadastrar_usuario(raiz_usuarios, raiz_idiomas, codigo_usuario, nome_usuario, codigo_idioma)
 
-resultado = buscar_usuario_com_idioma(raiz_usuarios, raiz_idiomas, codigo_usuario_busca)
+        print("\nUsuário cadastrado com sucesso!")
 
-if resultado is not None:
-    usuario, idioma = resultado
+    elif opcao == "2":
+        print("\n BUSCAR USUÁRIO ")
 
-    print("\nUsuário encontrado:")
-    print("Código: ", usuario.codigo)
-    print("Nome: ", usuario.nome)
-    print("Idioma aprendizado: ", idioma.descricao)
-    print("Nível atual: ", usuario.nivel_atual)
-    print("Pontuação total: ", usuario.pontuacao_total)
-else:
-    print("\nUsuário não encontrado")
+        while True:
+            try:
+                codigo_usuario = int(input("Digite o código do usuário: "))
+                break
+            except ValueError:
+                print("\nDigite apenas números.")
 
-usuarios = listar_usuarios(raiz_usuarios, [])
+        resultado = buscar_usuario_com_idioma(raiz_usuarios, raiz_idiomas, codigo_usuario)
 
-print("\nUsuários cadastrados:")
+        if resultado is None:
+            print("\nUsuário não encontrado.")
 
-for usuario in usuarios:
-    print(
-        usuario.codigo,
-        "-",
-        usuario.nome,
-        "- idioma:",
-        usuario.codigo_idioma_aprendizado
-    )
+        else:
+            usuario, idioma = resultado
 
-# tamanho = os.path.getsize("dados/idiomas.dat")
-# print("\nTamanho do arquivo:", tamanho)
+            print("\nUsuário encontrado:")
+            print("Código:", usuario.codigo)
+            print("Nome:", usuario.nome)
+            print("Idioma de aprendizado:", idioma.descricao)
+            print("Nível atual:", usuario.nivel_atual)
+            print("Pontuação total:", usuario.pontuacao_total)
 
-codigo_licao = int(input("\nDigite o código da lição para buscar: "))
+    elif opcao == "3":
+        print("\n LISTAR USUÁRIOS ")
 
-resultado = buscar_licao_com_idioma(raiz_licoes, raiz_idiomas, codigo_licao)
+        usuarios = listar_usuarios(raiz_usuarios, [])
 
-if resultado is not None:
-    licao, idioma = resultado
+        if len(usuarios) == 0:
+            print("\nNenhum usuário cadastrado.")
 
-    print("\nLição encontrada: ")
-    print("Código da lição: ", licao.cod_licao)
-    print("Idioma: ", idioma.descricao)
-    print("Total de níveis: ", licao.total_niveis)
-else:
-    print("\nLição não encontrada")
+        else:
+            print()
 
-exercicios = listar_exercicios(raiz_exercicios, [])
-print("\nExercícios disponíveis: ")
+            for usuario in usuarios:
+                idioma = buscar_idioma(
+                    raiz_idiomas,
+                    usuario.codigo_idioma_aprendizado
+                )
 
-for exercicio in exercicios:
-    print(exercicio.cod_exercicio, "-", exercicio.descricao)
+                print(
+                    "Código:", usuario.codigo,
+                    "| Nome:", usuario.nome,
+                    "| Idioma:", idioma.descricao,
+                    "| Nível:", usuario.nivel_atual,
+                    "| Pontuação:", usuario.pontuacao_total
+                )
 
-codigo_exercicio = int(input("\nDigite o código do exercício para buscar: "))
+    elif opcao == "4":
+        print("\n PRATICAR EXERCÍCIOS")
 
-resultado = buscar_exercicio_com_idioma(raiz_exercicios, raiz_licoes, raiz_idiomas, codigo_exercicio)
+        while True:
+            try:
+                codigo_usuario = int(input("Digite o código do usuário: "))
+                break
+            except ValueError:
+                print("\nDigite apenas números.")
 
-if resultado is not None:
-    exercicio, licao, idioma = resultado
+        usuario = buscar_usuario(raiz_usuarios, codigo_usuario)
 
-    print("\nExercício encontrado: ")
-    print("Código do exercício: ", exercicio.cod_exercicio)
-    print("Código da lição: ", exercicio.cod_licao)
-    print("Idioma: ", idioma.descricao)
-    print("Nível de dificuldade: ", exercicio.nivel_dificuldade)
-    print("Descrição: ", exercicio.descricao)
-    print("Opções: ", exercicio.opcoes_resposta)
-    print("Resposta correta: ", exercicio.resposta_correta)
-    print("Pontuação: ", exercicio.pontuacao)
-else:
-    print("\nExercício não encontrado!")
+        if usuario is None:
+            print("\nUsuário não encontrado.")
+            continue
 
-codigo_usuario_teste = int(input("\nDigite o código do usuário para testar a prática: "))
+        if usuario.ordem_conclusao != 0:
+            print("\nVocê já concluiu este idioma!")
+            continue
 
-codigo_exercicio_teste = int(input("Digite o código do exercício: "))
+        idioma = buscar_idioma(raiz_idiomas, usuario.codigo_idioma_aprendizado)
 
-pode_praticar = pode_praticar_exercicio(raiz_usuarios, raiz_exercicios, codigo_usuario_teste, codigo_exercicio_teste)
+        todos_exercicios = listar_exercicios(raiz_exercicios,[])
 
-if pode_praticar:
-    print("\nUsuário pode praticar esse exercício!")
-else:
-    print("\nUsuário não pode praticar!")
+        exercicios_nivel = []
 
-codigo_usuario_teste = int(
-    input("\nDigite o código do usuário para responder: ")
-)
+        for exercicio in todos_exercicios:
+            licao = buscar_licao(raiz_licoes, exercicio.cod_licao)
 
-codigo_exercicio_teste = int(
-    input("Digite o código do exercício: ")
-)
+            if (
+                licao is not None
+                and licao.cod_idioma == usuario.codigo_idioma_aprendizado
+                and exercicio.nivel_dificuldade == usuario.nivel_atual
+            ):
+                exercicios_nivel.append(exercicio)
 
-resposta_usuario = input(
-    "Digite sua resposta: "
-)
+        if len(exercicios_nivel) == 0:
+            print("\nNenhum exercício disponível para este nível.")
+            continue
 
-resultado = responder_exercicio(
-    raiz_usuarios,
-    raiz_exercicios,
-    codigo_usuario_teste,
-    codigo_exercicio_teste,
-    resposta_usuario
-)
+        print("\nUsuário:", usuario.nome)
+        print("Idioma:", idioma.descricao)
+        print("Nível atual:", usuario.nivel_atual)
+        print("Pontuação atual:", usuario.pontuacao_total)
 
-if resultado:
-    usuario_atualizado = buscar_usuario(
-        raiz_usuarios,
-        codigo_usuario_teste
-    )
+        for numero, exercicio in enumerate(
+            exercicios_nivel,
+            start=1
+        ):
+            print(
+                f"\n--- EXERCÍCIO {numero} DE {len(exercicios_nivel)} ---"
+            )
 
-    print("\nResposta processada!")
-    print("Pontuação atual:", usuario_atualizado.pontuacao_total)
-else:
-    print("\nNão foi possível responder esse exercício.")
+            print(exercicio.descricao)
+
+            for numero_opcao, opcao_resposta in enumerate(
+                exercicio.opcoes_resposta,
+                start=1
+            ):
+                print(
+                    numero_opcao,
+                    "-",
+                    opcao_resposta
+                )
+
+            while True:
+                try:
+                    escolha = int(input("\nEscolha uma alternativa: "))
+
+                    if 1 <= escolha <= len(exercicio.opcoes_resposta):
+                        break
+
+                    print("\nAlternativa inválida. Tente novamente.")
+                except ValueError:
+                    print("\nDigite apenas o número da alternativa.")
+
+            resposta_usuario = exercicio.opcoes_resposta[
+                escolha - 1
+            ]
+
+            acertou = (
+                resposta_usuario.strip().lower()
+                == exercicio.resposta_correta.strip().lower()
+            )
+
+            resultado_resposta = responder_exercicio(
+                raiz_usuarios,
+                raiz_exercicios,
+                raiz_licoes,
+                codigo_usuario,
+                exercicio.cod_exercicio,
+                resposta_usuario
+            )
+
+            if not resultado_resposta:
+                print("\nNão foi possível processar o exercício.")
+                continue
+
+            usuario = buscar_usuario(
+                raiz_usuarios,
+                codigo_usuario
+            )
+
+            if acertou:
+                print("\nResposta correta!")
+            else:
+                print("\nResposta incorreta!")
+                print(
+                    "Resposta correta:",
+                    exercicio.resposta_correta
+                )
+
+            print(
+                "Pontuação atual:",
+                usuario.pontuacao_total
+            )
+
+        cod_licao = exercicios_nivel[0].cod_licao
+
+        resultado_final = finalizar_rodada(
+            raiz_usuarios,
+            raiz_licoes,
+            codigo_usuario,
+            cod_licao
+        )
+
+        if resultado_final is False:
+            print("\nNão foi possível finalizar a rodada.")
+            continue
+
+        promoveu, concluiu = resultado_final
+
+        usuario = buscar_usuario(
+            raiz_usuarios,
+            codigo_usuario
+        )
+
+        print("\n RESULTADO DA RODADA")
+        print(
+            "Pontuação atual:",
+            usuario.pontuacao_total
+        )
+
+        if concluiu:
+            print("\nParabéns!")
+            print("Você concluiu o idioma!")
+
+        elif promoveu:
+            print("\nParabéns!")
+            print(
+                "Você avançou para o nível",
+                usuario.nivel_atual
+            )
+
+        else:
+            pontuacao_necessaria = (
+                usuario.nivel_atual * 100
+            )
+
+            faltam = (
+                pontuacao_necessaria
+                - usuario.pontuacao_total
+            )
+
+            print("\nVocê permanece no nível atual.")
+            print(
+                "Faltam",
+                faltam,
+                "pontos para avançar."
+            )
+
+    elif opcao == "5":
+        print("\n RANKING")
+
+        ranking = gerar_ranking(
+            raiz_usuarios
+        )
+
+        if len(ranking) == 0:
+            print("\nNenhum usuário cadastrado.")
+            continue
+
+        for posicao, usuario in enumerate(
+            ranking,
+            start=1
+        ):
+            idioma = buscar_idioma(
+                raiz_idiomas,
+                usuario.codigo_idioma_aprendizado
+            )
+
+            print(
+                f"{posicao}º -",
+                usuario.nome,
+                "| Pontuação:",
+                usuario.pontuacao_total,
+                "| Nível:",
+                usuario.nivel_atual,
+                "| Idioma:",
+                idioma.descricao
+            )
+
+    elif opcao == "6":
+        print("\n EXCLUIR USUÁRIO")
+
+        while True:
+            try:
+                codigo_usuario = int(input("Digite o código do usuário que deseja excluir: "))
+                break
+            except ValueError:
+                print("\nDigite apenas números.")
+
+        usuario = buscar_usuario(
+            raiz_usuarios,
+            codigo_usuario
+        )
+
+        if usuario is None:
+            print("\nUsuário não encontrado.")
+            continue
+
+        print("\nUsuário encontrado:")
+        print("Código:", usuario.codigo)
+        print("Nome:", usuario.nome)
+        print("Pontuação:", usuario.pontuacao_total)
+
+        while True:
+            confirmacao = input("\nTem certeza que deseja excluir este usuário? (S/N): ").strip().lower()
+
+            if confirmacao in ["s", "n"]:
+                break
+
+            print("\nDigite apenas S ou N.")
+
+        if confirmacao  == "s":
+            raiz_usuarios = excluir_usuario(
+                raiz_usuarios,
+                codigo_usuario
+            )
+
+            print("\nUsuário excluído com sucesso!")
+
+        else:
+            print("\nExclusão cancelada.")
+
+    elif opcao == "7":
+        print("\nEncerrando o MaxLanguage...")
+        break
